1db4a48aa7eeee7a1f2ac0b80181132021b1452d
PC-202007101216\Administrator <daichang>
2020-10-23 16:39:27 +0800

    three 3d

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as THREE from 'three'
+// 引入样式
 import './App.css';
+import 'antd/dist/antd.css';
+import './assets/css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// 根组件
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
 
-export default App;
+    }
+    this.initThreeScene = this.initThreeScene.bind(this)
+  }
+
+  componentDidMount() {
+    this.initThreeScene()
+  }
+
+  // 初始化 3d 场景
+  initThreeScene() {
+    let scene = new THREE.Scene()
+    let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+    let render = new THREE.WebGLRenderer()
+
+    render.setSize(window.innerWidth, window.innerHeight)
+
+    let gridHelper = new THREE.GridHelper(200, 25)
+    scene.add(gridHelper)
+
+    const element = document.getElementById('app')
+    element.append(render.domElement)
+  }
+
+  render() {
+    return (
+        <div id='app'></div>
+    )
+  }
+};
+
